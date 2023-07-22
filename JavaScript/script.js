7fb4a0eb417f594f84fbb4d2b360ecfc0e33d6c7
Martinsgundi <martinsgundi1@gmail.com>
2023-07-22 19:26:56 +0100

    first commit

---
+++ JavaScript/script.js
@@ -0,0 +1,8 @@
+
+// Changes color of like button 
+const changeHeartColor = document.querySelector('.fa-heart')
+
+changeHeartColor.addEventListener('click', () => {
+
+    changeHeartColor.classList.toggle('heart-color')
+});
